ddeab73444654169fce9313d7aa596d28952010b
Santhosh <thoshsan2003@gmail.com>
2025-09-24 18:41:21 +0530

    Update pyproject.toml and setup.py for package configuration

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='instagram_bot',
     version='0.1.0',
-    packages=find_packages(include=[
+    packages=[
         'image_generator',
         'instagram_uploader',
         'flashcard_generator',
         'meme_prompt_generator'
-    ]),
+    ],
     # ... other arguments ...
 )
